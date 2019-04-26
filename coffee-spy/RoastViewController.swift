ba3612d7d55e2b5ca130cb9b7b45171409381302
Konstantin Klitenik <kostyan5@gmail.com>
2019-04-25 21:21:44 -0400

    Create Xcode project

---
+++ coffee-spy/RoastViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RoastViewController.swift
+//  coffee-spy
+//
+//  Created by Konstantin Klitenik on 4/25/19.
+//  Copyright © 2019 Konstantin Klitenik. All rights reserved.
+//
+
+import UIKit
+
+class RoastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ coffee-spy/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  coffee-spy
+//
+//  Created by Konstantin Klitenik on 4/25/19.
+//  Copyright © 2019 Konstantin Klitenik. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
